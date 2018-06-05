90c07498f44a57bbbae183216c290005cfb00e1a
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-06-05 13:59:01 -0400

    solution complete

--- models/rps_game.rb
+++ models/rps_game.rb
@@ -1,12 +1,40 @@
 class RPSGame
+  attr_accessor :play, :computer_play
 
-  # Write your code here!
+  Plays = ["rock", "paper", "scissors"]
+  
+  WinMap = {
+    "rock" => "scissors",
+    "paper" => "rock",
+    "scissors" => "paper"
+  }
 
-
-
-  class PlayTypeError < StandardError
-    # Don't be alarmed! This is a class set inside a class, but works just fine in Ruby
-    # In this case, we're creating a custom error that should be raised if RPSGame.new receives an invalid move as an argument
+  def initialize(play)
+    if RPSGame.valid_play?(play)
+      self.play = play
+      self.computer_play = Plays.sample
+    else
+      raise PlayTypeError
+    end
   end
 
+  def self.valid_play?(play)
+    play = play if play
+    Plays.include?(play)
+  end
+
+  def won?
+    WinMap[play] == computer_play
+  end
+
+  def tied?
+    play == computer_play
+  end
+
+  def lost?
+    !won? && !tied?
+  end
+
+  class PlayTypeError < Exception
+  end
 end
